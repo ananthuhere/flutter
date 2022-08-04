2ad815bd38c01737dbce3b1e950c2d516dde1fa6
ananthuhere <ananthakrishnan139@gmail.com>
2022-08-04 09:48:12 +0530

    adding files

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ananthakrishnan',
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("ananthakrishnan"),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sample_demo/app.dart';
+
+void main() {
+  runApp(App());
+}
